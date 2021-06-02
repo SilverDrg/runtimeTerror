9b6f35d08cf34a1610462a6cf2d757a291ceba0e
Ivan Icov <ivan.icov@student.um.si>
2021-06-03 00:48:49 +0200

    Recognized license plate now is printed on stdout

--- BackEnd/ObjectRecognition/license_plate.py
+++ BackEnd/ObjectRecognition/license_plate.py
@@ -19,23 +19,17 @@ image = cv2.imread(args["image"])
 #resize the image to 500
 image = imutils.resize(image, width=500)
 
-#display the original image
-cv2.imshow("Original image", image)
-cv2.waitKey(0)
-
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Grayscale image", gray)
-cv2.waitKey(0)
+#cv2.imshow("Grayscale image", gray)
+#cv2.waitKey(0)
 
 #reduce noise from the image, make it smooth
 gray = cv2.bilateralFilter(gray, 11, 17, 17)
-cv2.imshow("Smoothed image", gray)
-cv2.waitKey(0)
 
 #find the edges in the image
 edged = cv2.Canny(gray, 170, 200)
-cv2.imshow("Canny edge", edged)
-cv2.waitKey(0)
+#cv2.imshow("Canny edge", edged)
+#cv2.waitKey(0)
 
 #find the contours based on the images
 contours, new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -46,8 +40,8 @@ contours, new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_S
 # create a copy of the original image to draw all the contours
 image1 = image.copy()
 cv2.drawContours(image1, contours, -1, (0,255,0), 3)
-cv2.imshow("Canny after contouring", image1)
-cv2.waitKey(0)
+#cv2.imshow("Canny after contouring", image1)
+#cv2.waitKey(0)
 
 #sort the contours based on their areas and select top 30 areas, then reverse their order(from max to mix)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
@@ -58,8 +52,8 @@ NumberPlateCount = None
 #to draw the top 30 contours we will make a copy of our original image
 image2 = image.copy()
 cv2.drawContours(image2, contours, -1, (0,255,0), 3)
-cv2.imshow("Top 30 contours", image2)
-cv2.waitKey(0)
+#cv2.imshow("Top 30 contours", image2)
+#cv2.waitKey(0)
 
 #find the best possible contour of our expected number plate
 count = 0
@@ -78,16 +72,16 @@ for i in contours:
         break
     
 #draw a contour in the detect license plate image
-if(NumberPlateCount != None):
+if([NumberPlateCount] != None):
     cv2.drawContours(image, [NumberPlateCount], -1, (0,255,0), 3)
-    cv2.imshow("Final image", image)
-    cv2.waitKey(0)
+    #cv2.imshow("Final image", image)
+    #cv2.waitKey(0)
 
-    cv2.imshow("Cropped image", crop_image)
-    cv2.waitKey(0)
+    #cv2.imshow("Cropped image", crop_image)
+    #cv2.waitKey(0)
 
     text = tess.image_to_string(crop_image, lang='eng', config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3')
-    print("License plate number is: ", text)
+    print(text)
 
 else:
     print("No license plate detected")
